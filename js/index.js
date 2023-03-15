0c751a5469c04faf0ce038e2f6b9adaa03a5f490
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2023-03-15 11:15:33 +0100

    tekst

--- js/index.js
+++ js/index.js
@@ -1,16 +1,19 @@
 "use strict";
 
 const chat = {
-    author: "yourName",
-    init() {
-    },
-    sendMessage() {
-    },
-    fetchMessages() {
-    },
-    renderMessage(message) {
-    }
-
-}
-
+	author: "yourName",
+	init() {
+		this.fetchMessages();
+	},
+	sendMessage() {},
+	fetchMessages() {
+		fetch("https://dev2chat.onrender.com/messages")
+			.then((response) => response.json())
+			.then(function (data) {
+				console.log(data);
+				document.querySelector("#messageContainer").innerHTML = data[0].message;
+			});
+	},
+	renderMessage(message) {},
+};
 chat.init();
